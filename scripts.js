5603f32f7033cb1b0d9df31197e03af363db5376
PiligalC0dD3 <michelpiligal@gmail.com>
2025-02-19 23:51:46 -0300

    conversão no cosole

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ const convertButton = document.querySelector(".convert-button")
 function convertValues() {    
     const inputCurrencyValue = document.querySelector(".input-valores").value
 
-    console.log(inputCurrencyValue)
+    const dolarToday = 5.72
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    console.log(convertedValue)
 }
 
 convertButton.addEventListener("click", convertValues) /*Aqui é o ouvinte */
