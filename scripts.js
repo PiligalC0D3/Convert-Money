88e7fd6da32618761f1fa9eb376e5d8a7ffe1e90
PiligalC0dD3 <michelpiligal@gmail.com>
2025-02-20 22:52:28 -0300

    nova function

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues() {    
     const inputCurrencyValue = document.querySelector(".input-valores").value
+    const currentValueToConvert = document.querySelector(".current-currency").value /* Valor em Real*/
+    const currentValueToConverted = document.querySelector(".current-currency").value /* Outras moedas */
+
 
     const dolarToday = 5.72
 
