2ccb8ec687325622610570e4487a97af863fce4a
PiligalC0dD3 <michelpiligal@gmail.com>
2025-02-24 00:58:25 -0300

    traduzindo as classes

--- scripts.js
+++ scripts.js
@@ -1,19 +1,18 @@
 const convertButton = document.querySelector(".convert-button") 
 
 function convertValues() {    
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToCurent = document.querySelector(".currency-value-to-convert").value /* Valor em Real*/
-    const currencyValueToConverted = document.querySelector(".currency-value") /* Outras moedas */
+    const inputValores = document.querySelector(".input-valores").value 
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert").value /* Valor em Real*/
+    const currencyValueConverted = document.querySelector(".currency-value") ;/* Outras moedas */
 
     const dolarToday = 5.72
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    const convertedValue = inputValores / dolarToday 
 
-    inputCurrencyValueToConvert.innerHTML = inputCurrencyValue
+    currencyValueToConvert.innerHTML = inputValores 
     
-
     console.log(convertedValue) 
 }
 
-convertButton.addEventListener("click", convertValues) /*Aqui é o ouvinte */
+convertButton.addEventListener("click", convertValues)  /*Aqui é o ouvinte */
 
