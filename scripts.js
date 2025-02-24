11f1cf8969345047183dca33bd5f6a0e58fea441
PiligalC0dD3 <michelpiligal@gmail.com>
2025-02-24 11:11:13 -0300

    padronizando

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 const convertButton = document.querySelector(".convert-button") 
 
 function convertValues() {    
-    const inputValores = document.querySelector(".input-valores").value 
+    const inputValue = document.querySelector(".input-value").value 
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert").value /* Valor em Real*/
     const currencyValueConverted = document.querySelector(".currency-value") ;/* Outras moedas */
 
     const dolarToday = 5.72
 
-    const convertedValue = inputValores / dolarToday 
+    const convertedValue = inputValue / dolarToday 
 
-    currencyValueToConvert.innerHTML = inputValores 
+    currencyValueToConvert.innerHTML = inputValue 
     
     console.log(convertedValue) 
 }
