f040d0da864cd2337d92eb17bb9c804e65095963
PiligalC0dD3 <michelpiligal@gmail.com>
2025-02-25 23:50:53 -0300

    Erro encontrado

--- scripts.js
+++ scripts.js
@@ -2,14 +2,15 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues() {    
     const inputCurrencyValue = document.querySelector(".input-currency").value 
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert").value /* Valor em Real*/
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") /* Valor em Real*/
     const currencyValueConverted = document.querySelector(".currency-value") /* Outras moedas */
 
     const dolarToday = 5.72
 
     const convertedValue = inputCurrencyValue / dolarToday 
 
-    currencyValueToConvert.innerHTML = inputCurrencyValue 
+    currencyValueToConvert.innerHTML = inputCurrencyValue
+    currencyValueConverted.innerHTML = convertedValue.toFixed(2)
     
     console.log(convertedValue) 
 }
